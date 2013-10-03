b52ac261dde2c63151908b0ce9cc3f156b9ebc04
Andre Little <ALittle@Fullsail.edu>
2013-10-03 16:46:33 -0500

    Added my launch.png image to MiU project 1

---
+++ Little_Andre_Project1/js/JSON.js
@@ -0,0 +1,22 @@
+/*
+Andre Little
+Class: MiU
+Term: 1310
+Completed Date: 10/03/2013
+Project 1: Mega Weight Lost Project - Feature, Search, Browse
+*/
+
+var json = {
+	"testMeal1": {
+		"dates": ["Date:", "2013-09-25"],
+		"times": ["Meal Time:", "Breakfast"],
+		"itemname": ["Name:", "Sausage"],
+		"fatgrams": ["Grams of Fat:", "2.5"],
+		"carbgrams": ["Grams of Carbs:", "5"],
+		"programs": ["Grams of Protein:", ".5"],
+		"size": ["Serving Size:", "1"],
+		"additional": ["Add Item:", "No"],
+		"notes": ["Notes about Meal:", "Turkey Linked" ]
+	}
+	
+}
