d2ad8365174630ba89999662dc733e531b2916e3
Andre Little <ALittle@Fullsail.edu>
2013-10-10 09:22:14 -0500

    Added my Project 1 Documents to Project 2

---
+++ Little_Andre_Project2/Gold/js/JSON.js
@@ -0,0 +1,233 @@
+/*
+Andre Little
+Class: MiU
+Term: 1310
+Completed Date: 10/03/2013
+Project 1: Mega Weight Lost Project - Feature, Search, Browse
+
+*/
+
+var json = {
+	"testMeal1": {
+		"dates": ["Date:", "2013-09-25"],
+		"times": ["Meal Time:", "Breakfast"],
+		"itemname": ["Name:", "Sausage"],
+		"fatgrams": ["Grams of Fat:", "2.5"],
+		"carbgrams": ["Grams of Carbs:", "5"],
+		"programs": ["Grams of Protein:", ".5"],
+		"size": ["Serving Size:", "1"],
+		"additional": ["Add Item:", "No"],
+		"notes": ["Notes about Meal:", "Turkey Linked" ]
+	},
+	"testMeal2": {
+		"dates": ["Date:", "2013-12-25"],
+		"times": ["Meal Time:", "Breakfast"],
+		"itemname": ["Name:", "Pancakes"],
+		"fatgrams": ["Grams of Fat:", "8"],
+		"carbgrams": ["Grams of Carbs:", "4"],
+		"programs": ["Grams of Protein:", "2"],
+		"size": ["Serving Size:", "3"],
+		"additional": ["Add Item:", "Yes"],
+		"notes": ["Notes about Meal:", "" ]
+	},
+	"testMeal3": {
+		"dates": ["Date:", "2013-22-25"],
+		"times": ["Meal Time:", "Dinner"],
+		"itemname": ["Name:", "Rice"],
+		"fatgrams": ["Grams of Fat:", "2"],
+		"carbgrams": ["Grams of Carbs:", "5"],
+		"programs": ["Grams of Protein:", "1.5"],
+		"size": ["Serving Size:", "1"],
+		"additional": ["Add Item:", "No"],
+		"notes": ["Notes about Meal:", "cooked" ]
+	},
+	"testMeal4": {
+		"dates": ["Date:", "2013-10-25"],
+		"times": ["Meal Time:", "Lunch"],
+		"itemname": ["Name:", "Burger"],
+		"fatgrams": ["Grams of Fat:", "10"],
+		"carbgrams": ["Grams of Carbs:", "5"],
+		"programs": ["Grams of Protein:", "1"],
+		"size": ["Serving Size:", "1"],
+		"additional": ["Add Item:", "No"],
+		"notes": ["Notes about Meal:", "Turkey Burger" ]
+	},
+	"testMeal5": {
+		"dates": ["Date:", "2011-01-25"],
+		"times": ["Meal Time:", "Breakfast"],
+		"itemname": ["Name:", "Coffee"],
+		"fatgrams": ["Grams of Fat:", "6"],
+		"carbgrams": ["Grams of Carbs:", "5"],
+		"programs": ["Grams of Protein:", "5"],
+		"size": ["Serving Size:", "1"],
+		"additional": ["Add Item:", "No"],
+		"notes": ["Notes about Meal:", "Black" ]
+	},
+	"testMeal6": {
+		"dates": ["Date:", "2013-08-21"],
+		"times": ["Meal Time:", "Lunch"],
+		"itemname": ["Name:", "chips"],
+		"fatgrams": ["Grams of Fat:", "3"],
+		"carbgrams": ["Grams of Carbs:", "2"],
+		"programs": ["Grams of Protein:", "5"],
+		"size": ["Serving Size:", "1"],
+		"additional": ["Add Item:", "Yes"],
+		"notes": ["Notes about Meal:", "Hot Cheetoes" ]
+	},
+	"testMeal7": {
+		"dates": ["Date:", "2013-05-25"],
+		"times": ["Meal Time:", "Dinner"],
+		"itemname": ["Name:", "Dressing"],
+		"fatgrams": ["Grams of Fat:", "12"],
+		"carbgrams": ["Grams of Carbs:", "5"],
+		"programs": ["Grams of Protein:", "5"],
+		"size": ["Serving Size:", "1"],
+		"additional": ["Add Item:", "Yes"],
+		"notes": ["Notes about Meal:", "Turkey" ]
+	},
+	"testMeal8": {
+		"dates": ["Date:", "2013-02-25"],
+		"times": ["Meal Time:", "Lunch"],
+		"itemname": ["Name:", "Subway"],
+		"fatgrams": ["Grams of Fat:", "5"],
+		"carbgrams": ["Grams of Carbs:", "5"],
+		"programs": ["Grams of Protein:", "5"],
+		"size": ["Serving Size:", "1"],
+		"additional": ["Add Item:", "No"],
+		"notes": ["Notes about Meal:", "Tuna" ]
+	},
+	"testMeal9": {
+		"dates": ["Date:", "1986-10-01"],
+		"times": ["Meal Time:", "Breakfast"],
+		"itemname": ["Name:", "Baby Milk"],
+		"fatgrams": ["Grams of Fat:", "0"],
+		"carbgrams": ["Grams of Carbs:", "0"],
+		"programs": ["Grams of Protein:", "0"],
+		"size": ["Serving Size:", "1"],
+		"additional": ["Add Item:", "No"],
+		"notes": ["Notes about Meal:", "Formula" ]
+	},
+	"testMeal10": {
+		"dates": ["Date:", "2013-09-25"],
+		"times": ["Meal Time:", "Dinner"],
+		"itemname": ["Name:", "Greens"],
+		"fatgrams": ["Grams of Fat:", "25"],
+		"carbgrams": ["Grams of Carbs:", "15"],
+		"programs": ["Grams of Protein:", "5"],
+		"size": ["Serving Size:", "1"],
+		"additional": ["Add Item:", "Yes"],
+		"notes": ["Notes about Meal:", "" ]
+	},
+	"testMeal11": {
+		"dates": ["Date:", "2012-03-01"],
+		"times": ["Meal Time:", "Breakfast"],
+		"itemname": ["Name:", "Cereal"],
+		"fatgrams": ["Grams of Fat:", "12"],
+		"carbgrams": ["Grams of Carbs:", "5"],
+		"programs": ["Grams of Protein:", "15"],
+		"size": ["Serving Size:", "1"],
+		"additional": ["Add Item:", "No"],
+		"notes": ["Notes about Meal:", "captain Crunch" ]
+	},
+	"testMeal12": {
+		"dates": ["Date:", "2013-07-15"],
+		"times": ["Meal Time:", "Dinner"],
+		"itemname": ["Name:", "McDonalds"],
+		"fatgrams": ["Grams of Fat:", "6"],
+		"carbgrams": ["Grams of Carbs:", "8"],
+		"programs": ["Grams of Protein:", "5"],
+		"size": ["Serving Size:", "1"],
+		"additional": ["Add Item:", "No"],
+		"notes": ["Notes about Meal:", "McChicken and a Fry" ]
+	},
+	"testMeal13": {
+		"dates": ["Date:", "2001-09-25"],
+		"times": ["Meal Time:", "Lunch"],
+		"itemname": ["Name:", "Taco Bell"],
+		"fatgrams": ["Grams of Fat:", "25"],
+		"carbgrams": ["Grams of Carbs:", "5"],
+		"programs": ["Grams of Protein:", "5"],
+		"size": ["Serving Size:", "12"],
+		"additional": ["Add Item:", "No"],
+		"notes": ["Notes about Meal:", "12 pack" ]
+	},
+	"testMeal14": {
+		"dates": ["Date:", "2012-09-05"],
+		"times": ["Meal Time:", "Breakfast"],
+		"itemname": ["Name:", "Sausage"],
+		"fatgrams": ["Grams of Fat:", "2"],
+		"carbgrams": ["Grams of Carbs:", "5"],
+		"programs": ["Grams of Protein:", "15"],
+		"size": ["Serving Size:", "1"],
+		"additional": ["Add Item:", "No"],
+		"notes": ["Notes about Meal:", "Beef Linked" ]
+	},
+	"testMeal15": {
+		"dates": ["Date:", "2011-09-25"],
+		"times": ["Meal Time:", "Breakfast"],
+		"itemname": ["Name:", "Oatmeal"],
+		"fatgrams": ["Grams of Fat:", ".5"],
+		"carbgrams": ["Grams of Carbs:", ".5"],
+		"programs": ["Grams of Protein:", "15"],
+		"size": ["Serving Size:", "1"],
+		"additional": ["Add Item:", "No"],
+		"notes": ["Notes about Meal:", "Cinnamon" ]
+	},
+	"testMeal16": {
+		"dates": ["Date:", "2013-09-28"],
+		"times": ["Meal Time:", "Dinner"],
+		"itemname": ["Name:", "Ham"],
+		"fatgrams": ["Grams of Fat:", "2.5"],
+		"carbgrams": ["Grams of Carbs:", "5"],
+		"programs": ["Grams of Protein:", ".5"],
+		"size": ["Serving Size:", "1"],
+		"additional": ["Add Item:", "No"],
+		"notes": ["Notes about Meal:", "" ]
+	},
+	"testMeal17": {
+		"dates": ["Date:", "2014-09-25"],
+		"times": ["Meal Time:", "Breakfast"],
+		"itemname": ["Name:", "Latté"],
+		"fatgrams": ["Grams of Fat:", "15"],
+		"carbgrams": ["Grams of Carbs:", "15"],
+		"programs": ["Grams of Protein:", "15"],
+		"size": ["Serving Size:", "1"],
+		"additional": ["Add Item:", "No"],
+		"notes": ["Notes about Meal:", "" ]
+	},
+	"testMeal18": {
+		"dates": ["Date:", "2015-09-25"],
+		"times": ["Meal Time:", "Breakfast"],
+		"itemname": ["Name:", "grapes"],
+		"fatgrams": ["Grams of Fat:", "5"],
+		"carbgrams": ["Grams of Carbs:", ".5"],
+		"programs": ["Grams of Protein:", ".5"],
+		"size": ["Serving Size:", "1"],
+		"additional": ["Add Item:", "No"],
+		"notes": ["Notes about Meal:", "Green" ]
+	},	
+	"testMeal19": {
+		"dates": ["Date:", "2003-09-25"],
+		"times": ["Meal Time:", "Lunch"],
+		"itemname": ["Name:", "Chicken"],
+		"fatgrams": ["Grams of Fat:", "2.5"],
+		"carbgrams": ["Grams of Carbs:", "1.5"],
+		"programs": ["Grams of Protein:", "1.5"],
+		"size": ["Serving Size:", "2"],
+		"additional": ["Add Item:", "No"],
+		"notes": ["Notes about Meal:", "Baked" ]
+	},
+	"testMeal20": {
+		"dates": ["Date:", "2008-09-25"],
+		"times": ["Meal Time:", "Breakfast"],
+		"itemname": ["Name:", "Apple"],
+		"fatgrams": ["Grams of Fat:", "0"],
+		"carbgrams": ["Grams of Carbs:", "0"],
+		"programs": ["Grams of Protein:", "0"],
+		"size": ["Serving Size:", "1"],
+		"additional": ["Add Item:", "No"],
+		"notes": ["Notes about Meal:", "" ]
+	}
+}
+
+
